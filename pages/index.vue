c561fe4d3c9829ff85629caff06c3a3e4e095998
bobbyray1005 <braythriz4@gmail.com>
2019-10-30 15:35:23 +0200

    :zap:

--- pages/index.vue
+++ pages/index.vue
@@ -83,7 +83,7 @@ export default {
         );
         return;
       }
-      localStorage.setItem("privateKey", this.privateKey);
+      localStorage.setItem("privateKey", this.privateKey.trim());
       this.$router.push("wallet");
     },
     gotoCreate() {
